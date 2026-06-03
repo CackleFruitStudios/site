fb1dbb3859ce7f0c1503b7b125f82684c27dc67e
CackleFruitStudios <cacklefruitstudios@gmail.com>
2026-06-03 13:43:43 -0400

    Add gallery-save API endpoint handler

---
+++ 80studios/api/gallery-save.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+
+  if (req.method !== "POST") {
+
+    return res.status(405).json({
+      error: "Method not allowed"
+    });
+  }
+
+  console.log(
+    "gallery-save called"
+  );
+
+  return res.status(200).json({
+    success: true,
+    message: "gallery-save endpoint online"
+  });
+}
